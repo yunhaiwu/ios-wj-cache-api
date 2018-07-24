c2501645e3b22f9a1f85c05337ab62ba7862f66a
halayun <halayun@qq.com>
2018-07-24 08:17:50 +0800

    update

---
+++ Example/WJCacheAPI-example/WJCacheAPI-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WJCacheAPI-example
+//
+//  Created by 吴云海 on 16/9/6.
+//  Copyright © 2016年 wuyunhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
